aad7025e61d7dc4f40f8247b3881cbcf589bf6d5
echoplus <qazwsxedc219@qq.com>
2019-08-01 14:57:39 +0800

    新增数据库配置

--- src/test/java/com/liupeng/sell/LoggerTest.java
+++ src/test/java/com/liupeng/sell/LoggerTest.java
@@ -1,6 +1,8 @@
 package com.liupeng.sell;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LoggerTest {
 
+    @Before
+    public void testBefore(){
+        System.out.println("before=================");
+    }
+
+
     @Test
     public void test() {
-        log.info("info....");
+        String name = "echo";
+        Integer age = 12;
+        log.info("info....name:{},age:{}", name, age);
         log.debug("debug...");
         log.error("error...");
+    }
+
+    @After
+    public void testAfter(){
+        System.out.println("after!!=================");
     }
 }
